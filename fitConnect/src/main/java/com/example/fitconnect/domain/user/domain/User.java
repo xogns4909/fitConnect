1596bfc66ed29275c4896f10f60441889dae7bcf
xogns4909 <xogns4909@naver.com>
2024-01-16 20:45:43 +0900

    refactor : 회원 등록시 이미 있는 회원이면 유저 정보 전달

--- fitConnect/src/main/java/com/example/fitconnect/domain/user/domain/User.java
+++ fitConnect/src/main/java/com/example/fitconnect/domain/user/domain/User.java
@@ -12,6 +12,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Builder;
 import lombok.Getter;
 @Entity
 @Table(name = "users")
--- fitConnect/src/main/java/com/example/fitconnect/service/user/UserRegisterService.java
+++ fitConnect/src/main/java/com/example/fitconnect/service/user/UserRegisterService.java
@@ -15,23 +15,14 @@ public class UserRegisterService {
     private final UserRepository userRepository;
 
 
+
     @Transactional
-    public User registerNewUser(UserRegistrationDto registrationDto) {
-        checkIfUserAlreadyExists(registrationDto.getEmail());
-
-        User newUser = registrationDto.toEntity();
-
-        try {
-            return userRepository.save(newUser);
-        } catch (Exception e) {
-            throw new BusinessException(ErrorMessages.REGISTRATION_FAILED);
-        }
-    }
-
-    private void checkIfUserAlreadyExists(String email) {
-        userRepository.findByUserBaseInfo_Email(email).ifPresent(user -> {
-            throw new BusinessException(ErrorMessages.EMAIL_ALREADY_EXISTS);
-        });
+    public User registerOrGetUser(UserRegistrationDto registrationDto) {
+        return userRepository.findByUserBaseInfo_Email(registrationDto.getEmail())
+                .orElseGet(() -> {
+                    User newUser = registrationDto.toEntity();
+                    return userRepository.save(newUser);
+                });
     }
 }
 
