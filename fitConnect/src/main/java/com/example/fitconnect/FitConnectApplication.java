6e7fed34a7a346b948a8b9864e52ef03cca302a5
xogns4909 <xogns4909@naver.com>
2024-01-16 10:07:41 +0900

    feat: 개발환경 구축

---
+++ fitConnect/src/main/java/com/example/fitconnect/FitConnectApplication.java
@@ -0,0 +1,13 @@
+package com.example.fitconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitConnectApplication.class, args);
+    }
+
+}
