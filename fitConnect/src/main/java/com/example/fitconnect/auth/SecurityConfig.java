8834e7824d890b15b3ec8d4836093b2a32cbc12d
xogns4909 <xogns4909@naver.com>
2024-01-18 13:21:24 +0900

    feat : build.gradle 업데이트 및 Spring Security생성

---
+++ fitConnect/src/main/java/com/example/fitconnect/auth/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.example.fitconnect.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().permitAll()
+                )
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers((headers -> headers
+                        .frameOptions(FrameOptionsConfig::disable))
+                )
+                .build();
+    }
+}
