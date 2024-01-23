26ae5104b495a7091f6c2ee20f82172dff0472c7
xogns4909 <xogns4909@naver.com>
2024-01-23 15:27:58 +0900

    feat : ExerciseEventFindService 구현 및 테스트

---
+++ fitConnect/src/main/java/com/example/fitconnect/service/event/ExerciseEventFindService.java
@@ -0,0 +1,24 @@
+package com.example.fitconnect.service.event;
+
+import com.example.fitconnect.domain.event.domain.Category;
+import com.example.fitconnect.domain.event.domain.ExerciseEvent;
+import com.example.fitconnect.repository.event.ExerciseEventRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ExerciseEventFindService {
+
+    private final ExerciseEventRepository exerciseEventRepository;
+
+
+    public Page<ExerciseEvent> findEvents(Category category, String description, int page) {
+        return exerciseEventRepository.findEventsWithConditions(category, description, page);
+    }
+}
+
+
---
+++ fitConnect/src/test/java/com/example/fitconnect/service/event/ExerciseEventFindServiceTest.java
@@ -0,0 +1,50 @@
+package com.example.fitconnect.service.event;
+
+import com.example.fitconnect.domain.event.domain.Category;
+import com.example.fitconnect.domain.event.domain.ExerciseEvent;
+import com.example.fitconnect.repository.event.ExerciseEventRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+public class ExerciseEventFindServiceTest {
+
+    @Mock
+    private ExerciseEventRepository exerciseEventRepository;
+
+    @InjectMocks
+    private ExerciseEventFindService exerciseEventFindService;
+    
+
+    @Test
+    void whenFindEvents_thenReturnsPageOfEvents() {
+        Category category = Category.SOCCER;
+        String description = "Soccer match in Seoul";
+        int page = 0;
+        ExerciseEvent event1 = new ExerciseEvent();
+        ExerciseEvent event2 = new ExerciseEvent();
+
+        List<ExerciseEvent> events = Arrays.asList(event1, event2);
+        Page<ExerciseEvent> expectedPage = new PageImpl<>(events, PageRequest.of(page, 10), events.size());
+
+        given(exerciseEventRepository.findEventsWithConditions(category, description, page))
+                .willReturn(expectedPage);
+
+        Page<ExerciseEvent> result = exerciseEventFindService.findEvents(category, description, page);
+
+        assertThat(result).isEqualTo(expectedPage);
+    }
+}
