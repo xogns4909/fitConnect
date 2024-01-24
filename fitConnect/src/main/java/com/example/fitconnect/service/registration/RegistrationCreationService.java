9d7b9c2395d4891643c3656fb5bab5e69e28274a
xogns4909 <xogns4909@naver.com>
2024-01-24 10:52:40 +0900

    feat : 운동 신청 서비스 구현 밑 테스트

---
+++ fitConnect/src/main/java/com/example/fitconnect/service/registration/RegistrationCreationService.java
@@ -0,0 +1,46 @@
+package com.example.fitconnect.service.registration;
+
+import com.example.fitconnect.config.error.ErrorMessages;
+import com.example.fitconnect.config.exception.EntityNotFoundException;
+import com.example.fitconnect.domain.event.domain.ExerciseEvent;
+import com.example.fitconnect.domain.registration.Registration;
+import com.example.fitconnect.domain.registration.RegistrationStatus;
+import com.example.fitconnect.domain.user.domain.User;
+import com.example.fitconnect.repository.event.ExerciseEventRepository;
+import com.example.fitconnect.repository.registration.RegistrationRepository;
+import com.example.fitconnect.repository.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class RegistrationCreationService {
+
+    private final RegistrationRepository registrationRepository;
+    private final UserRepository userRepository;
+    private final ExerciseEventRepository exerciseEventRepository;
+
+    @Transactional
+    public Registration createRegistration(Long userId, Long eventId) {
+        User user = findUser(userId);
+        ExerciseEvent event = findEvent(eventId);
+
+        Registration registration = new Registration(user,event);
+
+        return registrationRepository.save(registration);
+    }
+
+    private ExerciseEvent findEvent(Long eventId) {
+        ExerciseEvent event = exerciseEventRepository.findById(eventId)
+                .orElseThrow(() -> new EntityNotFoundException(ErrorMessages.EVENT_NOT_FOUND));
+        return event;
+    }
+
+    private User findUser(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException(ErrorMessages.USER_NOT_FOUND));
+        return user;
+    }
+
+}
