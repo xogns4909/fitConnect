3b4b22cd58c1fe7c369d9dd42bd8eb074f524bfe
xogns4909 <xogns4909@naver.com>
2024-01-25 10:59:28 +0900

    feat : 리뷰 동적 조회 기능 추가 및 테스트

---
+++ fitConnect/src/main/java/com/example/fitconnect/repository/review/ReviewRepositoryImpl.java
@@ -0,0 +1,38 @@
+package com.example.fitconnect.repository.review;
+
+import com.example.fitconnect.domain.review.QReview;
+import com.example.fitconnect.domain.review.Review;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+
+public class ReviewRepositoryImpl implements CustomReviewRepository {
+
+    private final JPAQueryFactory queryFactory;
+    private final QReview qReview = QReview.review;
+
+    public ReviewRepositoryImpl(EntityManager em) {
+        this.queryFactory = new JPAQueryFactory(em);
+    }
+
+    @Override
+    public Page<Review> findReviews(int page, int size, String sortBy) {
+        JPAQuery<Review> query = queryFactory
+                .selectFrom(qReview)
+                .offset((page - 1) * size)
+                .limit(size);
+
+        if ("rating".equals(sortBy)) {
+            query.orderBy(qReview.rating.asc());
+        }
+        query.orderBy(qReview.id.asc());
+
+        List<Review> content = query.fetch();
+        long total = query.fetchCount();
+        return new PageImpl<>(content, PageRequest.of(page - 1, size), total);
+    }
+}
---
+++ fitConnect/src/test/java/com/example/fitconnect/repository/review/ReviewRepositoryTest.java
@@ -0,0 +1,69 @@
+package com.example.fitconnect.repository.review;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.example.fitconnect.domain.event.domain.ExerciseEvent;
+import com.example.fitconnect.domain.review.Review;
+import com.example.fitconnect.domain.user.domain.Role;
+import com.example.fitconnect.domain.user.domain.User;
+import com.example.fitconnect.domain.user.domain.UserBaseInfo;
+import com.example.fitconnect.repository.event.ExerciseEventRepository;
+import com.example.fitconnect.repository.review.ReviewRepository;
+import com.example.fitconnect.repository.user.UserRepository;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+public class ReviewRepositoryTest {
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    private ExerciseEventRepository exerciseEventRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeEach
+    public void setUp() {
+
+        User user = new User(new UserBaseInfo("test@naver.com", "nickname",
+                "url"), Role.MEMBER);
+        ExerciseEvent exerciseEvent = new ExerciseEvent();
+
+        userRepository.saveAndFlush(user);
+        exerciseEventRepository.saveAndFlush(exerciseEvent);
+        Review review1 = new Review("Content 1", 4.5, user, exerciseEvent);
+        Review review2 = new Review("Content 2", 3.7, user, exerciseEvent);
+        Review review3 = new Review("Content 3", 5.0, user, exerciseEvent);
+
+        reviewRepository.saveAll(List.of(review1, review2, review3));
+    }
+
+    @Test
+    public void testFindReviewsByRating() {
+        Page<Review> reviews = reviewRepository.findReviews(1, 10, "rating");
+
+        assertThat(reviews).isNotNull();
+        assertThat(reviews.getContent()).hasSize(3);
+        assertThat(reviews.getContent()).extracting(Review::getRating)
+                .containsExactly(3.7, 4.5, 5.0);
+    }
+
+    @Test
+    public void testFindReviewsByDefault() {
+        Page<Review> reviews = reviewRepository.findReviews(1, 10, "default");
+
+        assertThat(reviews).isNotNull();
+        assertThat(reviews.getContent()).hasSize(3);
+        assertThat(reviews.getContent().get(0).getId()).isEqualTo(1L);
+    }
+}
