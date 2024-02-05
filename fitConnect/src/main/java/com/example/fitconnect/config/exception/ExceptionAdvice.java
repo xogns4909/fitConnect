626ff94d14d18bd832987d0b565f5c26e5d7799f
xogns4909 <xogns4909@naver.com>
2024-02-05 12:23:27 +0900

    fix : ControllerAdvice 에서 RestControllerAdvice 로 변경

--- fitConnect/src/main/java/com/example/fitconnect/config/exception/ExceptionAdvice.java
+++ fitConnect/src/main/java/com/example/fitconnect/config/exception/ExceptionAdvice.java
@@ -5,8 +5,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ExceptionAdvice {
 
     @ExceptionHandler(BusinessException.class)
