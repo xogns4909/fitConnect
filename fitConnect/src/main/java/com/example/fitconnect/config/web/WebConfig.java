1f3b4b2776ffb5511967243b77f799294344c549
xogns4909 <xogns4909@naver.com>
2024-02-05 12:22:51 +0900

    fix : 기존에 추가하지 않은 CurrentUserId 및 WebConfig 추가

---
+++ fitConnect/src/main/java/com/example/fitconnect/config/web/WebConfig.java
@@ -0,0 +1,16 @@
+package com.example.fitconnect.config.web;
+
+import com.example.fitconnect.config.resolver.CurrentUserIdArgumentResolver;
+import java.util.List;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new CurrentUserIdArgumentResolver());
+    }
+}
