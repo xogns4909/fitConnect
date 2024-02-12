759ecdd7be65c1a789c33c31d640870f49f1a805
xogns4909 <xogns4909@naver.com>
2024-02-12 20:57:12 +0900

    feat : 리뷰 삭제 로직 추가 및 상세보기 추가

--- fitConnect/frontend/src/components/ReviewList.js
+++ fitConnect/frontend/src/components/ReviewList.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { ListGroup } from 'react-bootstrap';
-
+import { ListGroup,Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 const ReviewsList = () => {
   const [reviews, setReviews] = useState([]);
-
+  const navigate = useNavigate();
   useEffect(() => {
     fetchReviews();
   }, []);
@@ -13,8 +13,18 @@ const ReviewsList = () => {
     try {
       const response = await axios.get(`/mypage/reviews`);
       setReviews(response.data.content);
+      console.log(response.data.content);
     } catch (error) {
       console.error('Error fetching reviews:', error);
+    }
+  };
+
+  const deleteReview = async (reviewId) => {
+    try {
+      await axios.delete(`/api/reviews/${reviewId}`);
+      fetchReviews();
+    } catch (error) {
+      console.error('Error deleting review:', error);
     }
   };
 
@@ -22,8 +32,11 @@ const ReviewsList = () => {
       <ListGroup>
         {reviews.map(review => (
             <ListGroup.Item key={review.id}>
-              <h5>{review.title}</h5>
-              <p>{review.content}</p>
+              <h5>{review.eventTitle}</h5>
+              <p>내용: {review.reviewContent}</p>
+              <p>평점: {review.rating}</p>
+              <Button variant="primary" onClick={() => navigate(`/events/${review.eventId}`)}>상세 보기</Button>{' '}
+              <Button variant="danger" onClick={() => deleteReview(review.reviewId)}>삭제</Button>
             </ListGroup.Item>
         ))}
       </ListGroup>
