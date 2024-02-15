3cdbf937721d0f716158841643e97a026c8ecf7b
xogns4909 <xogns4909@naver.com>
2024-02-15 21:44:42 +0900

    feat : 최대 신청 인원 예외처리 추가

--- fitConnect/src/main/java/com/example/fitconnect/service/registration/RegistrationApprovalService.java
+++ fitConnect/src/main/java/com/example/fitconnect/service/registration/RegistrationApprovalService.java
@@ -1,9 +1,16 @@
 package com.example.fitconnect.service.registration;
 
+import static com.example.fitconnect.config.error.ErrorMessages.EVENT_MAX_PARTICIPANTS_REACHED;
+import static com.example.fitconnect.config.error.ErrorMessages.EVENT_NOT_FOUND;
+
 import com.example.fitconnect.config.error.ErrorMessages;
+import com.example.fitconnect.config.exception.BusinessException;
 import com.example.fitconnect.config.exception.EntityNotFoundException;
+import com.example.fitconnect.domain.event.domain.ExerciseEvent;
 import com.example.fitconnect.domain.registration.Registration;
+import com.example.fitconnect.domain.registration.RegistrationStatus;
 import com.example.fitconnect.repository.registration.RegistrationRepository;
+import com.example.fitconnect.service.event.ExerciseEventFindService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,10 +21,21 @@ public class RegistrationApprovalService {
 
     private final RegistrationRepository registrationRepository;
 
+    private final ExerciseEventFindService exerciseEventFindService;
+
+    public long countApprovedRegistrations(Long eventId) {
+        return registrationRepository.countByExerciseEventIdAndStatus(eventId,
+                RegistrationStatus.APPROVED);
+    }
+
     @Transactional
-    public void approveRegistration(Long registrationId, Long adminId) {
+    public void approveRegistration(Long registrationId, Long userId, Long eventId) {
         Registration registration = findRegistration(registrationId);
-        registration.approve(adminId);
+        ExerciseEvent event = findEvent(eventId);
+        long approvedCount = countApprovedRegistrations(eventId);
+
+        validationMaxParticipants(approvedCount, event);
+        registration.approve(userId);
     }
 
     @Transactional
@@ -26,10 +44,22 @@ public class RegistrationApprovalService {
         registration.deny(adminId);
     }
 
+    private void validationMaxParticipants(long approvedCount, ExerciseEvent event) {
+        if (approvedCount >= event.getRegistrationPolicy().getMaxParticipants()) {
+            throw new BusinessException(EVENT_MAX_PARTICIPANTS_REACHED);
+        }
+    }
+
     private Registration findRegistration(Long registrationId) {
         return registrationRepository.findById(registrationId)
                 .orElseThrow(
                         () -> new EntityNotFoundException(ErrorMessages.REGISTRATION_NOT_FOUND));
+    }
+
+
+    private ExerciseEvent findEvent(Long eventId) {
+        return exerciseEventFindService.findEventByEventId(eventId)
+                .orElseThrow(() -> new BusinessException(EVENT_NOT_FOUND));
     }
 
 }
