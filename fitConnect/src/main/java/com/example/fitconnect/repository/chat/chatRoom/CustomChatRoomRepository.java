8c663519da537c484656e023cda6f2c96f71b328
xogns4909 <xogns4909@naver.com>
2024-02-15 18:46:32 +0900

    feat :  유저 아이디와 이벤트아디로 채팅방 조회 쿼리 및 테스트 추가

--- fitConnect/src/main/java/com/example/fitconnect/repository/chat/chatRoom/CustomChatRoomRepository.java
+++ fitConnect/src/main/java/com/example/fitconnect/repository/chat/chatRoom/CustomChatRoomRepository.java
@@ -2,12 +2,15 @@ package com.example.fitconnect.repository.chat.chatRoom;
 
 import com.example.fitconnect.domain.chat.domain.ChatMessage;
 import com.example.fitconnect.domain.chat.domain.ChatRoom;
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface CustomChatRoomRepository {
 
     Page<ChatRoom> findByChatRoomId(Long userId, Pageable pageable);
+
+    Optional<ChatRoom> findByUserIdAndExerciseEventId(Long userId, Long exerciseEventId);
 
 }
 
