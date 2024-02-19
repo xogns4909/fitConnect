af37f8594a51fd4f6c40c0951d79bc9c82faf08b
xogns4909 <xogns4909@naver.com>
2024-02-19 19:59:07 +0900

    feat: : 리뷰 작성후 리뷰페이지로 이동

--- fitConnect/frontend/src/components/Registration.js
+++ fitConnect/frontend/src/components/Registration.js
@@ -8,6 +8,7 @@ const RegistrationButton = ({ eventId }) => {
       const response = await axios.post('/api/registrations/' + eventId, null);
       alert('신청이 완료되었습니다.');
       console.log(response.data);
+      window.location.href = '/mypage?tab=registeredEvents'
     } catch (error) {
       const errorMessage = error.response?.data; // 서버로부터 오류 코드 받기
       let message = '신청 중 오류가 발생했습니다.';
