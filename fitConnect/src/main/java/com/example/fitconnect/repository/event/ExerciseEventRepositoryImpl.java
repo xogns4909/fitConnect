f5a3412280e85a8d094ab4bb76b4a13454896760
xogns4909 <xogns4909@naver.com>
2024-02-19 20:04:33 +0900

    fix  : 게시글 리스트 호출시 시간순 정렬 로직 추가

--- fitConnect/src/main/java/com/example/fitconnect/repository/event/ExerciseEventRepositoryImpl.java
+++ fitConnect/src/main/java/com/example/fitconnect/repository/event/ExerciseEventRepositoryImpl.java
@@ -112,6 +112,7 @@ public class ExerciseEventRepositoryImpl implements CustomExerciseEventRepositor
             QExerciseEvent qExerciseEvent, Predicate predicate, int page) {
         List<ExerciseEvent> events = queryFactory.selectFrom(qExerciseEvent)
                 .where(predicate)
+                .orderBy(qExerciseEvent.createdAt.desc())
                 .offset((long) page * PAGE_SIZE)
                 .limit(PAGE_SIZE)
                 .fetch();
