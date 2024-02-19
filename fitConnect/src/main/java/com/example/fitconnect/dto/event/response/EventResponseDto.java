889664e35195f77021e0edc429dedcc8da640e22
xogns4909 <xogns4909@naver.com>
2024-02-19 17:36:01 +0900

    refactor :  디자인 변경 밑 작성 시간 추가

--- fitConnect/src/main/java/com/example/fitconnect/dto/event/response/EventResponseDto.java
+++ fitConnect/src/main/java/com/example/fitconnect/dto/event/response/EventResponseDto.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.cglib.core.Local;
 
 @Getter
 public class EventResponseDto {
@@ -19,17 +20,20 @@ public class EventResponseDto {
 
     private LocalDateTime endTime;
 
+    private LocalDateTime writeTime;
+
     protected EventResponseDto() {
     }
 
     public EventResponseDto(Long id, String title, String category, String organizerNickname,
-            LocalDateTime startTime, LocalDateTime endTime) {
+            LocalDateTime startTime, LocalDateTime endTime,LocalDateTime writeTime) {
         this.id = id;
         this.title = title;
         this.category = category;
         this.organizerNickname = organizerNickname;
         this.startTime = startTime;
         this.endTime = endTime;
+        this.writeTime = writeTime;
     }
 
     public static EventResponseDto toDto(ExerciseEvent exerciseEvent) {
@@ -39,7 +43,8 @@ public class EventResponseDto {
                 exerciseEvent.getCategory().getName(),
                 exerciseEvent.getOrganizer().getUserBaseInfo().getNickname(),
                 exerciseEvent.getEventDetail().getStartDate(),
-                exerciseEvent.getEventDetail().getEndDate()
+                exerciseEvent.getEventDetail().getEndDate(),
+                exerciseEvent.getCreatedAt()
         );
     }
 }
