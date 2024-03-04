924accd3810b6c9877354977778498d71d7b4feb
xogns4909 <xogns4909@naver.com>
2024-03-04 21:15:18 +0900

    fix : 이벤트 디테일 요청시 게시글과 리뷰 각자 요청로직으로 변경 및 리뷰 페이징 추가

--- fitConnect/frontend/src/components/EventDetail.js
+++ fitConnect/frontend/src/components/EventDetail.js
@@ -1,63 +1,87 @@
 import React, { useEffect, useState } from 'react';
 import axiosInstance from '../global/axiosConfig';
-import { Button, Card, ListGroup,ListGroupItem } from 'react-bootstrap';
+import { Button, Card, ListGroup, ListGroupItem, Pagination } from 'react-bootstrap';
 import RegistrationButton from '../components/Registration';
 import CreateChatRoom from "../components/CreateChatRoom";
 import { translateCity, translateCategory } from './Translations';
+
 const EventDetail = ({ eventId }) => {
   const [event, setEvent] = useState(null);
+  const [reviews, setReviews] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
   const [error, setError] = useState('');
 
   useEffect(() => {
     const fetchEventDetail = async () => {
       try {
         const response = await axiosInstance.get(`/api/events/${eventId}/detail`);
-        console.log("response = ",response.data)
         setEvent(response.data);
-
       } catch (err) {
         console.log(err.response);
         setError(err.response?.data?.message || '이벤트 상세 정보를 가져오는데 실패했습니다.');
       }
     };
+
     fetchEventDetail();
   }, [eventId]);
+
+  useEffect(() => {
+    const fetchReviews = async () => {
+      try {
+        const response = await axiosInstance.get(`/api/reviews/events/${eventId}`, {
+          params: {
+            page: currentPage ,
+            size: 10,
+          },
+        });
+        setReviews(response.data.content);
+        setTotalPages(response.data.totalPages);
+      } catch (err) {
+        console.log(err.response);
+        // 리뷰 로딩 실패 처리
+      }
+    };
+
+    fetchReviews();
+  }, [eventId, currentPage]);
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber); // 페이지 번호 변경 처리
+  };
 
   if (error) return <div>오류: {error}</div>;
   if (!event) return <div>로딩 중...</div>;
 
-
-
   return (
       <>
-      <Card className="my-4 shadow">
-        <Card.Body>
-          <Card.Title className="display-4">{event.title}</Card.Title>
-          <ListGroup className="list-group-flush">
-            <ListGroup.Item>운동 시간: {new Date(event.startTime).toLocaleString()} ~ {new Date(event.endTime).toLocaleString()}</ListGroup.Item>
-            <ListGroup.Item>신청 기간: {new Date(event.registrationStart).toLocaleString()} ~ {new Date(event.registrationEnd).toLocaleString()}</ListGroup.Item>
-            <ListGroup.Item>최대 참가자: {event.maxParticipants}</ListGroup.Item>
-            <ListGroup.Item>종목: {translateCategory(event.category)}</ListGroup.Item>
-            <ListGroup.Item>위치: {translateCity(event.city)}, {event.address}</ListGroup.Item>
-          </ListGroup>
-          <Card.Text className="mt-4">{event.description}</Card.Text>
-          <div className="mt-4">
-            <RegistrationButton eventId={eventId} />
-            <CreateChatRoom eventId={eventId} />
-          </div>
-        </Card.Body>
-      </Card>
+        <Card className="my-4 shadow">
+          <Card.Body>
+            <Card.Title className="display-4">{event.title}</Card.Title>
+            <ListGroup className="list-group-flush">
+              <ListGroup.Item>운동 시간: {new Date(event.startTime).toLocaleString()} ~ {new Date(event.endTime).toLocaleString()}</ListGroup.Item>
+              <ListGroup.Item>신청 기간: {new Date(event.registrationStart).toLocaleString()} ~ {new Date(event.registrationEnd).toLocaleString()}</ListGroup.Item>
+              <ListGroup.Item>최대 참가자: {event.maxParticipants}</ListGroup.Item>
+              <ListGroup.Item>종목: {translateCategory(event.category)}</ListGroup.Item>
+              <ListGroup.Item>위치: {translateCity(event.city)}, {event.address}</ListGroup.Item>
+            </ListGroup>
+            <Card.Text className="mt-4">{event.description}</Card.Text>
+            <div className="mt-4">
+              <RegistrationButton eventId={eventId} />
+              <CreateChatRoom eventId={eventId} />
+            </div>
+          </Card.Body>
+        </Card>
         <Card className="mb-4 shadow">
           <Card.Header>리뷰</Card.Header>
           <ListGroup variant="flush">
-            {event.reviewResponseDtoList.length > 0 ? (
-                event.reviewResponseDtoList.map((review) => (
+            {reviews.length > 0 ? (
+                reviews.map((review) => (
                     <ListGroupItem key={review.id}>
                       <strong>평점: {review.rating}</strong>
-                      <p>{review.reviewContent}</p>
-
+                      <p>{review.reviewContent}<  /p>
                       <div className="review-footer">
-                        <small>작성자: {review.nickName}  </small>
+                        <small>작성자: {review.nickName}</small>
                       </div>
                     </ListGroupItem>
                 ))
@@ -65,6 +89,15 @@ const EventDetail = ({ eventId }) => {
                 <ListGroupItem>리뷰가 없습니다.</ListGroupItem>
             )}
           </ListGroup>
+          {totalPages > 1 && (
+              <Pagination className="justify-content-center">
+                {[...Array(totalPages).keys()].map(number => (
+                    <Pagination.Item key={number + 1} active={number + 1 === currentPage} onClick={() => handlePageChange(number + 1)}>
+                      {number + 1}
+                    </Pagination.Item>
+                ))}
+              </Pagination>
+          )}
         </Card>
       </>
   );
