5edfa296df4ed70ad5f1e2cfc7b4956558f57a4c
xogns4909 <xogns4909@naver.com>
2024-03-04 20:54:48 +0900

    fix: 마이페이지 UI 개선 및 데이터 로딩 방식 변경

--- fitConnect/frontend/src/page/MyPage.js
+++ fitConnect/frontend/src/page/MyPage.js
@@ -1,64 +1,39 @@
 import React from 'react';
-import { Container, Tab, Nav, Row, Col } from 'react-bootstrap';
-import { useLocation, useNavigate } from 'react-router-dom';
-import ReviewsList from '../components/ReviewList';
-import RegisteredEventsList from '../components/RegisteredEventsList';
-import MyEventsList from '../components/MyEventsList';
+import { Container, Nav, Row, Col } from 'react-bootstrap';
+import { Outlet, Link, useLocation, Navigate } from 'react-router-dom';
 import NavbarComponent from '../components/Navbar';
-import UserInfo from "../components/UserInfo";
-import '../style/Mypage.css';
 
 const MyPage = () => {
   const location = useLocation();
-  const navigate = useNavigate();
-  const query = new URLSearchParams(location.search);
-  const activeTab = query.get('tab') || 'userInfo';
-
-  const handleSelect = (key) => {
-    navigate(`?tab=${key}`);
-  };
+  const currentTab = location.pathname.split("/")[2];
 
   return (
       <>
         <NavbarComponent />
         <Container className="my-page mt-3">
           <h2>마이페이지</h2>
-          <Tab.Container activeKey={activeTab} onSelect={handleSelect} defaultActiveKey="userInfo">
-            <Row>
-              <Col sm={3}>
-                <Nav variant="pills" className="flex-column mt-3">
-                  <Nav.Item>
-                    <Nav.Link eventKey="userInfo">유저 정보</Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                    <Nav.Link eventKey="reviews">리뷰</Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                    <Nav.Link eventKey="registeredEvents">등록한 이벤트</Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                    <Nav.Link eventKey="myEvents">내 이벤트</Nav.Link>
-                  </Nav.Item>
-                </Nav>
-              </Col>
-              <Col sm={9}>
-                <Tab.Content>
-                  <Tab.Pane eventKey="userInfo">
-                    <UserInfo />
-                  </Tab.Pane>
-                  <Tab.Pane eventKey="reviews">
-                    <ReviewsList />
-                  </Tab.Pane>
-                  <Tab.Pane eventKey="registeredEvents">
-                    <RegisteredEventsList />
-                  </Tab.Pane>
-                  <Tab.Pane eventKey="myEvents">
-                    <MyEventsList />
-                  </Tab.Pane>
-                </Tab.Content>
-              </Col>
-            </Row>
-          </Tab.Container>
+          <Row>
+            <Col sm={3}>
+              <Nav variant="pills" className="flex-column mt-3">
+                <Nav.Item>
+                  <Nav.Link as={Link} to="user-info">유저 정보</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link as={Link} to="reviews">리뷰</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link as={Link} to="registered-events">등록한 이벤트</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link as={Link} to="my-events">내 이벤트</Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Col>
+            <Col sm={9}>
+              {!currentTab && <Navigate to="user-info" replace />}
+              <Outlet />
+            </Col>
+          </Row>
         </Container>
       </>
   );
