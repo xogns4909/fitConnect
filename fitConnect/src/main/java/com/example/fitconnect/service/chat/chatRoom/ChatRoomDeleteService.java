0fffbbbd9e9bc9241b3435a0082d5a6324f192e4
xogns4909 <xogns4909@naver.com>
2024-03-10 16:11:14 +0900

    fix : 채팅방 삭제시 메세지 삭제 로직 추가

--- fitConnect/src/main/java/com/example/fitconnect/service/chat/chatRoom/ChatRoomDeleteService.java
+++ fitConnect/src/main/java/com/example/fitconnect/service/chat/chatRoom/ChatRoomDeleteService.java
@@ -1,9 +1,14 @@
 package com.example.fitconnect.service.chat.chatRoom;
 
+import com.example.fitconnect.domain.chat.domain.ChatMessage;
+import com.example.fitconnect.dto.chatMessage.response.ChatMessageResponseDto;
 import com.example.fitconnect.global.error.ErrorMessages;
 import com.example.fitconnect.global.exception.EntityNotFoundException;
 import com.example.fitconnect.domain.chat.domain.ChatRoom;
 import com.example.fitconnect.repository.chat.chatRoom.ChatRoomRepository;
+import com.example.fitconnect.service.chat.chatMessage.ChatMessageDeleteService;
+import com.example.fitconnect.service.chat.chatMessage.ChatMessageFindService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,16 +20,28 @@ public class ChatRoomDeleteService {
 
     private final ChatRoomRepository chatRoomRepository;
 
+    private final ChatMessageFindService chatMessageFindService;
+
+    private final ChatMessageDeleteService chatMessageDeleteService;
+
     public void deleteChatRoom(Long userId, Long chatRoomId) {
 
 
         ChatRoom chatRoom = chatRoomFindService.findChatRoomByChatRoomId(chatRoomId)
                 .orElseThrow(() -> new EntityNotFoundException(ErrorMessages.CHATROOM_NOT_FOUND));
 
+        deleteMessages(chatRoomId);
         chatRoom.validateCreator(userId);
         chatRoomRepository.delete(chatRoom);
 
     }
+
+    private void deleteMessages(Long chatRoomId) {
+        List<ChatMessage> chatMessages = chatMessageFindService.findChatMessagesByChatRoomId(
+                chatRoomId);
+        chatMessageDeleteService.deleteMessages(chatMessages);
+    }
+
 
 }
 
